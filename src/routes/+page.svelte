3f7d9be36f4246c467f8667c08960a9b291ac66d
zphyrj <46908127+zphyrj@users.noreply.github.com>
2023-06-19 06:31:23 -0700

    updated description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,23 +4,40 @@
 </script>
 
 <main>
-	<h1>How to lead a social movement in the digital age:</h1>
+	<h1>How Viral Spikes Shape Digital Movements</h1>
 	<p>
-		So, you want to start a social movement. You've got the passion, you've got the motivation, and
-		you've got a team. But how do you get your message out there? How do you get people to listen?
-		How do you get people to care?
+		Before social media, movements met up in person, disrupted events with protests, and made noise
+		in real life. Today, most of that happens online. There are still protests and there are still
+		real live meetings and discussions but those are just the tips of the iceberg. The majority of
+		the conversations and collaboration toward change occur online today.
 	</p>
 	<p>
-		Social media platforms such as Twitter, Instagram, and TikTok are run by recommendation
-		algorithms. These algorithms actively strive to keep users on the app for as long as it possibly
-		can. They do this by running experiments millions of times every second on billions of users
-		worldwide. The algorithm then learns what content keeps people - especially people similar to
-		you - on the platform for longer, watching more ads. Then it shows you that content which got
-		others to stay on their app for longer. Don't believe me? Look at this visualization of Twitter:
+		We created our visualization to explore how different social movements captured the attention of
+		social media users, or in other words, to examine their visibility and reach. This is because
+		traditional media used to operate as a monopoly on public attention - it could decide not to
+		cover a movement for ideological or corporate reasons. Thus movements relied on mass media to
+		publicize their cause and events to tell their story. Today, mass media can distort or
+		trivialize causes of movements.
 	</p>
+	<p>
+		We wanted to help future movement leaders figure out how to navigate what happens below the
+		surface. What they, in large part, don’t get to see. Sure they see their tweets but that is only
+		the slice that algorithms want to show them. By displaying a dashboard of people and their
+		tweeting behavior, it makes it obvious that these movements are driven by attention-driven
+		algorithms. This emphasizes the need for movements to both be prepared for spikes in engagement
+		and for the hijacking of movements.
+	</p>
+	<h2>Visualization (WIP)</h2>
 	{#if browser}
 		<Vis />
 	{/if}
+	<h2>How we incorporated Data Feminism</h2>
+	<p>By making the visualization interractive, we allow the user to explore the data themselves.</p>
+	<p>
+		We ideally want to curate certain key moments throughout the visualization to emphasize hoe
+		movements shift over time. Unfortunately, we ran out of time to do so by the deadline but we
+		still intend to add this to the visualization at some point.
+	</p>
 </main>
 
 <style>
