dfebb8fb231da1085a257af983d83dc50b1e3e02
zphyrj <46908127+zphyrj@users.noreply.github.com>
2023-07-03 19:42:50 -0700

    work on caching

--- publish.sh
+++ publish.sh
@@ -1,6 +1,3 @@
-rm -rf ../gh-publish/humn-55-final
+rm -rf ../gh-publish/humn-55-final/*
 cp -r ./build/* ../gh-publish/humn-55-final
-cd ../gh-publish/humn-55-final
-git add .
-git commit -m "Publishing"
-git push origin gh-pages
+cd ../gh-publish/humn-55-final && git add . && git commit -m "Publishing" && git push origin gh-pages
