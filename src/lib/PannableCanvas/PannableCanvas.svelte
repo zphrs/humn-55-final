4469e5cf76fb4b042aa5756a5cf3f9d8104d6953
zphyrj <46908127+zphyrj@users.noreply.github.com>
2023-08-12 21:50:58 -0700

    raised the min zoom level

--- src/lib/PannableCanvas/PannableCanvas.svelte
+++ src/lib/PannableCanvas/PannableCanvas.svelte
@@ -44,7 +44,7 @@
 	export let pointersWritableProxy: Writable<PointersDict> = writable({})
 	export let pPan: (e: CustomEvent<PanEvent>, ctx: CanvasRenderingContext2D) => boolean = () => true
 	export let context: ContextWrapper<Context2D> | undefined = undefined
-	export let minZoom = 1000
+	export let minZoom = 2000
 	export let maxZoom = 100000
 
 	export let cameraRect: Rect = new Rect(-0.5, -0.5, 0.5, 0.5)
