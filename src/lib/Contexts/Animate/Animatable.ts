8eb42f063870541f03e5f2d6a3af98af500d813e
zphyrj <46908127+zphyrj@users.noreply.github.com>
2023-09-16 06:49:31 -0700

    fixed animatable types

--- src/lib/Contexts/Animate/Animatable.ts
+++ src/lib/Contexts/Animate/Animatable.ts
@@ -1,4 +1,3 @@
-import { dictTreeToGenerator } from '$lib/Utils/DictTree'
 import { clamp, lerpFunc } from '$lib/Utils/vec2'
 import { broadcast, type Listener, type Listeners } from '../Listeners'
 import type { Interp } from './Interp'
@@ -24,6 +23,10 @@ type ChildrenOfRecursiveAnimatable<T> = {
 	[P in keyof T]: T[P] extends RecursiveAnimatable<unknown> ? T[P] : undefined
 }
 
+type Mask<T> = {
+	[P in keyof T]: T[P] | boolean
+}
+
 export type AnimationInfoWithoutChildren<Animating extends RecursiveAnimatable<unknown>> = {
 	time: number
 	timingFunction: Interp
@@ -35,9 +38,9 @@ export type AnimationInfoWithoutChildren<Animating extends RecursiveAnimatable<u
 export type AnimationInfo<Animating extends RecursiveAnimatable<unknown>> =
 	AnimationInfoWithoutChildren<Animating> & {
 		children: {
-			[P in keyof Animating]: Animating[P] extends RecursiveAnimatable<unknown>
-				? AnimationInfo<Animating[P]>
-				: undefined
+			[P in keyof Animating]: Animating[P] extends number
+				? undefined
+				: AnimationInfo<RecursiveAnimatable<Animating[P]>>
 		}
 	}
 
@@ -64,16 +67,12 @@ function lerpAnimatable<Animating extends Animatable>(
 }
 
 function copyObject<T>(obj: T) {
-	const cpy = {} as T
-	for (const key in obj) {
-		cpy[key] = obj[key]
-	}
-	return cpy
+	return { ...obj }
 }
 export function createLocalAnimationInfo<Animating extends RecursiveAnimatable<unknown>>(
-	init: Animating,
+	init: LocalRecursiveAnimatable<Animating>,
 	timing: Interp,
-	bounds?: Bounds<Animating>
+	bounds?: LocalRecursiveAnimatable<Bounds<Animating>>
 ): AnimationInfoWithoutChildren<Animating> {
 	let initCpy = copyObject(init)
 	let boundsCpy = bounds ? copyObject(bounds) : undefined
@@ -82,7 +81,7 @@ export function createLocalAnimationInfo<Animating extends RecursiveAnimatable<u
 		timingFunction: timing,
 		from: initCpy,
 		to: null,
-		bounds: boundsCpy,
+		bounds: boundsCpy as Bounds<LocalRecursiveAnimatable<Animating>>,
 		startListeners: new Set(),
 		endListeners: new Set(),
 		bounceListeners: new Set(),
@@ -112,10 +111,11 @@ export function createAnimationInfo<Init extends RecursiveAnimatable<unknown>>(
 	bounds?: Bounds<Init>
 ): AnimationInfo<Init> {
 	const [anim, children] = separateChildren(init)
-	const info = createLocalAnimationInfo(anim, timing, bounds) as unknown as AnimationInfo<Init>
+	const [boundsAnim, boundsChildren] = separateChildren(bounds ?? {})
+	const info = createLocalAnimationInfo(anim, timing, boundsAnim) as unknown as AnimationInfo<Init>
 	info.children = {} as AnimationInfo<Init>['children']
 	for (const [key, child] of Object.entries(children)) {
-		info.children[key as keyof Init] = createAnimationInfo(
+		;(info.children as any)[key] = createAnimationInfo(
 			child as RecursiveAnimatable<unknown>,
 			timing,
 			bounds
@@ -211,7 +211,7 @@ function saveState<Animating extends RecursiveAnimatable<unknown>>(
 	info: AnimationInfo<Animating>,
 	state: LocalRecursiveAnimatable<Animating>
 ) {
-	info.from = copyObject(state) as Animating
+	info.from = copyObject(state)
 	info.to = null
 	info.time = 0
 }
@@ -223,7 +223,7 @@ export function getCurrentState<Animating extends RecursiveAnimatable<unknown>>(
 		return info.from
 	}
 	const progress = getProgress(info)
-	return lerpAnimatable(info.from, info.to, progress) as Animating
+	return lerpAnimatable(info.from, info.to, progress) as LocalRecursiveAnimatable<Animating>
 }
 
 export function getCurrentStateWithChildren<Animating extends RecursiveAnimatable<unknown>>(
@@ -232,8 +232,8 @@ export function getCurrentStateWithChildren<Animating extends RecursiveAnimatabl
 	const out = getCurrentState(info) as Animating
 	for (const [key, childInfo] of Object.entries(info.children)) {
 		out[key as keyof Animating] = getCurrentStateWithChildren(
-			childInfo as AnimationInfo<Animating[keyof Animating]>
-		)
+			childInfo as AnimationInfo<RecursiveAnimatable<unknown>>
+		) as Animating[keyof Animating]
 	}
 	return out
 }
@@ -251,8 +251,10 @@ export function updateAnimationInfo<Animating extends RecursiveAnimatable<unknow
 	}
 	let out = animationNeedsUpdate(info)
 	// update children
-	for (const childInfo of Object.values(info.children)) {
-		if (updateAnimationInfo(childInfo as AnimationInfo<Animating[keyof Animating]>, dt)) {
+	for (const childInfo of Object.values<AnimationInfo<RecursiveAnimatable<unknown>>>(
+		info.children as { [s: string]: AnimationInfo<RecursiveAnimatable<unknown>> }
+	)) {
+		if (updateAnimationInfo(childInfo, dt)) {
 			out = true
 		}
 	}
@@ -260,22 +262,53 @@ export function updateAnimationInfo<Animating extends RecursiveAnimatable<unknow
 		const newState = mergeDicts(info.from, info.to as object)
 		saveState<Animating>(info, newState)
 		broadcast(info.endListeners, info.from)
+		return true
 	}
 	return out
 }
 
 export function changeInterpFunction<Animating extends RecursiveAnimatable<unknown>>(
 	info: AnimationInfo<Animating>,
-	interp: Interp
+	interp: Interp,
+	mask: Partial<Mask<Animating>> = {} // assumes default of true
 ) {
 	info.timingFunction = interp
-	const { to } = info
-	if (!to) return
-	let completeTo = to as Partial<LocalRecursiveAnimatable<Animating>>
-	if (info.to) {
-		completeTo = mergeDicts(info.to, to)
-		saveState(info, getCurrentState(info))
-	}
+	let { to } = info
+	if (!to) to = {}
+	saveState(info, getCurrentState(info))
 	info.time = 0
-	info.to = completeTo
+	// update children
+	const filteredChildren = Object.entries(info.children).filter(
+		([key]) => mask[key as keyof typeof mask] !== false
+	) as [keyof Animating, AnimationInfo<RecursiveAnimatable<unknown>>][]
+	for (const childInfo of filteredChildren) {
+		changeInterpFunction(
+			childInfo[1] as AnimationInfo<Animatable>,
+			interp,
+			mask[childInfo[0] as keyof typeof mask] as Partial<Mask<Animatable>>
+		)
+	}
+}
+
+export function getInterpingTo<Animating extends RecursiveAnimatable<unknown>>(
+	info: AnimationInfo<Animating>
+) {
+	if (info.to === null) {
+		return info.from
+	}
+	return mergeDicts(info.to, info.from) as Animating
+}
+
+export function getInterpingToWithChildren<Animating extends RecursiveAnimatable<unknown>>(
+	info: AnimationInfo<Animating>
+): Animating {
+	const out = getInterpingTo(info) as Animating
+	for (const [key, childInfo] of Object.entries<AnimationInfo<RecursiveAnimatable<unknown>>>(
+		info.children as { [s: string]: AnimationInfo<RecursiveAnimatable<unknown>> }
+	)) {
+		out[key as keyof Animating] = getInterpingToWithChildren(
+			childInfo
+		) as Animating[keyof Animating]
+	}
+	return out
 }
